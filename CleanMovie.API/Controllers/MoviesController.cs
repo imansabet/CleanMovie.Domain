d6e9d35f376998f20d58189b773f6bcac28d62b6
iman <dev.imansabet@gmail.com>
2024-02-19 20:58:23 +0330

    Presentation Layer : CleanMovie.API Add Controller : MoviesController

---
+++ CleanMovie.API/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [HttpGet]
+        public ActionResult<List<Movie>> Get() 
+        {
+            var moviesFromService = _movieService.GetAllMovies();
+            return Ok(moviesFromService);
+        }
+    }
+}
